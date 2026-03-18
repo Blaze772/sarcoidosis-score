f8d9c1a62d29bc064631f016157616a734b92337
KKing <79348807+Blaze772@users.noreply.github.com>
2026-03-18 12:33:11 -0500

    Delete PythonProject directory

--- PythonProject/Mad Libs Game.py
+++
@@ -1,9 +0,0 @@
-Color = input("Enter your favorite color: ")
-Plural_noun= input("Enter a plural noun: ")
-Celebrity = input("Enter your favorite Celebrity: ")
-
-
-
-print(" Roses are " + Color )
-print(  Plural_noun + " are blue ")
-print(" I love " + Celebrity)
