f42bc78ae4fb6d9f4ae2b63d78aaa45cdea01122
KKing <79348807+Blaze772@users.noreply.github.com>
2026-03-26 17:24:04 -0500

    Add files via upload

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,32 +1,11 @@
-const CACHE_NAME = "sarcoidosis-pwa-v2";const URLS_TO_CACHE = [
-  "/",
-  "/manifest.json",
-  "/static/icon-192.png",
-  "/static/icon-512.png"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
-  );
-});
-
+self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    )
-  );
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.map(key => caches.delete(key)))
+        ).then(() => self.clients.claim())
+    );
 });
-
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
-      return cachedResponse || fetch(event.request);
-    })
-  );
+    event.respondWith(fetch(event.request));
 });
